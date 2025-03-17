d787ae9102400c18b29a593b12c3ef35a094a6cb
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-03-17 12:55:00 +0200

    features: add all section in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Why from "@/app/sections/home-page/WhyUseSection"
+import Why from "@/app/sections/home-page/WhyUseSection";
+import HeroSection from "@/app/sections/home-page/HeroSection";
+import AboutSection from "@/app/sections/home-page/AboutSection";
+import HowBuildSection from "@/app/sections/home-page/HowBuildSection";
+import ReviewsSection from "./sections/home-page/ReviewsSection";
+import FormSection from "./sections/home-page/FormSection";
+import PriceSection from "./sections/home-page/PriceSection";
 
 export default function Home() {
-  return <>
-  <Why/>
-  </>;
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <Why />
+      <HowBuildSection />
+      <ReviewsSection />
+      <PriceSection />
+      <FormSection />
+    </>
+  );
 }
