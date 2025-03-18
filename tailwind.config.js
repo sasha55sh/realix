f41017c8ea7d2f3e9478a355cecea0d22b7b33c2
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-03-18 17:08:33 +0200

    delete unnecessary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,6 @@ module.exports = {
         xl: ["22px", { lineHeight: "32px" }],
       },
       colors: {
-        darkLavander: "#734F96",
-        white: "#FFFFFF",
         lavenderBlue: "#C0B7E8",
         deepPurple: "#343045",
         pearlBlue: "#CAD1E9",
@@ -31,13 +29,10 @@ module.exports = {
         darkVioletGradient:
           "linear-gradient(90deg, #1E1B2D 0%, #A098CF 35%, #6B6290 69%, #1E1B2D 100%)",
         darkGradient: "linear-gradient(0deg, #21223C 0%, #0B0B26 100%)",
-        raindowGradient:
-          "linear-gradient(90deg, #B67EFD 0%, #F088D2 41%, #FFAD8B 100%)",
         grayGradient: "linear-gradient(90deg, #1B1C36 0%, #70717A 100%)",
         useCardGradient: "radial-gradient(circle, #433D60 30%, #211E2E 70%)",
         heroGradient: "radial-gradient(circle, #3A3456 0%, #211E2E 100%)",
         formGradient: "radial-gradient(circle, #403A5F 0%, #211E2E 100%)",
-        priceCardGradient: "linear-gradient(90deg, #1B1C36 0%, #70717A 100%)",
         circleGradient: "radial-gradient(circle, #C0B7E8 30%, #8176AF 70%) ",
       },
     },
